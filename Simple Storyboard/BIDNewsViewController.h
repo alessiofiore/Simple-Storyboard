21e966577303321f1ee25fe3b4d461e0abcfa9d4
alessiofiore <info@madbit.org>
2013-03-09 18:46:17 +0100

    Custom TableView

---
+++ Simple Storyboard/BIDNewsViewController.h
@@ -0,0 +1,13 @@
+//
+//  BIDNewsViewController.h
+//  Simple Storyboard
+//
+//  Created by Alessio Fiore on 09/03/13.
+//  Copyright (c) 2013 Ericsson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BIDNewsViewController : UITableViewController
+
+@end
--- Simple Storyboard/BIDNewsViewController.m
+++ Simple Storyboard/BIDNewsViewController.m
@@ -1,18 +1,18 @@
 //
-//  BIDTaskListController.m
+//  BIDNewsViewController.m
 //  Simple Storyboard
 //
-//  Created by Alessio Fiore on 04/03/13.
+//  Created by Alessio Fiore on 09/03/13.
 //  Copyright (c) 2013 Ericsson. All rights reserved.
 //
 
-#import "BIDTaskListController.h"
+#import "BIDNewsViewController.h"
 
-@interface BIDTaskListController ()
+@interface BIDNewsViewController ()
 @property (strong, nonatomic) NSArray *tasks;
 @end
 
-@implementation BIDTaskListController
+@implementation BIDNewsViewController
 
 - (id)initWithStyle:(UITableViewStyle)style
 {
@@ -26,16 +26,17 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-    self.title = @"Items";
-    self.tasks = @[@"Item 1",
-                   @"Item 2",
-                   @"Item 3"];
-
+    self.title = @"News";
+    self.tasks = @[@"News 1",
+                   @"News 2",
+                   @"News 3"];
     // Uncomment the following line to preserve selection between presentations.
     // self.clearsSelectionOnViewWillAppear = NO;
  
     // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
     // self.navigationItem.rightBarButtonItem = self.editButtonItem;
+    
+    
 }
 
 - (void)didReceiveMemoryWarning
@@ -55,24 +56,71 @@
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
 {
     // Return the number of rows in the section.
-    return [self.tasks count];
+    return [self.tasks count];;
 }
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
-{    
+{
     // Configure the cell...
     
-    NSString *identifier = @"plainCell";
+    NSString *identifier = @"newsCell";
     NSString *task = [self.tasks objectAtIndex:indexPath.row];
     
     UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:identifier];
     // Configure the cell...
-    UILabel *cellLabel = (UILabel *)[cell viewWithTag:1];    
+    UILabel *cellLabel = (UILabel *)[cell viewWithTag:1];
     NSMutableAttributedString *richTask = [[NSMutableAttributedString alloc] initWithString:task];
     cellLabel.attributedText = richTask;
     
+    UIImageView *imgView = (UIImageView *) [cell viewWithTag:2];
+//    NSData *receivedData = [NSData dataWithContentsOfURL:[NSURL URLWithString:data.imageLink]];
+    UIImage *image = [UIImage imageNamed:@"person.png"];
+    imgView.image = image;
+    
+    [imgView.layer setBorderColor: [[UIColor blackColor] CGColor]];
+    [imgView.layer setBorderWidth: 2.0];
+    
     return cell;
 }
+
+/*
+// Override to support conditional editing of the table view.
+- (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    // Return NO if you do not want the specified item to be editable.
+    return YES;
+}
+*/
+
+/*
+// Override to support editing the table view.
+- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    if (editingStyle == UITableViewCellEditingStyleDelete) {
+        // Delete the row from the data source
+        [tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationFade];
+    }   
+    else if (editingStyle == UITableViewCellEditingStyleInsert) {
+        // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+    }   
+}
+*/
+
+/*
+// Override to support rearranging the table view.
+- (void)tableView:(UITableView *)tableView moveRowAtIndexPath:(NSIndexPath *)fromIndexPath toIndexPath:(NSIndexPath *)toIndexPath
+{
+}
+*/
+
+/*
+// Override to support conditional rearranging of the table view.
+- (BOOL)tableView:(UITableView *)tableView canMoveRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    // Return NO if you do not want the item to be re-orderable.
+    return YES;
+}
+*/
 
 #pragma mark - Table view delegate
 
